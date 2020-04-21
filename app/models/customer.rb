1e23668685fe26fbfe3e32bffedb333eb20c1b91
illnull <lolshinepro@gmail.com>
2020-04-20 19:04:16 -0500

    validation for customer sign up

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,15 @@
 
 class Customer < ApplicationRecord
   has_secure_password
-  validates_presence_of :email, :password, :firstName, :lastName, :phoneNumber, :city, :country, :province_id
+  validates_presence_of :email, :password, :province_id, :address
+  validates :firstName, format: { with: /\A[a-zA-Z]+\z/,
+                                  message: 'only allows letters' }
+  validates :lastName, format: { with: /\A[a-zA-Z]+\z/,
+                                 message: 'only allows letters' }
+  validates :country, format: { with: /\A[a-zA-Z]+\z/,
+                                message: 'only allows letters' }
+  validates :city, format: { with: /\A[a-zA-Z]+\z/,
+                             message: 'only allows letters' }
+  validates :phoneNumber, numericality: true
   validates_uniqueness_of :email
 end
