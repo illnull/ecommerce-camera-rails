761cdf4131dbdee6529bef80e0297e85d31285d4
illnull <lolshinepro@gmail.com>
2020-04-22 23:55:52 -0500

    added search function for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,12 +19,21 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @products = Product.where('created_at > ?', 3.days.ago)
+    @products = Product.where("created_at > ?", 3.days.ago)
     respond_with(@products)
   end
 
   def by_categoryId
     @products = Product.where(category_id: params[:id])
+    respond_with(@products)
+  end
+
+  def search
+    if params[:categoryId] == "null"
+      @products = Product.where("name LIKE ?", "%#{params[:keyword]}")
+    else
+      @products = Product.where("category_id = ? AND name LIKE ?", params[:categoryId], "%#{params[:keyword]}%")
+    end
     respond_with(@products)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,30 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'contacts/view'
+  get "contacts/view"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   # delete :logout, to: 'customers#logout'
   # get :logged_in, to: 'customers#logged_in'
-  root to: 'static#home'
+  root to: "static#home"
 
   constraints ->(request) { request.format == :json } do
-    get '/categories', to: 'categories#categories'
-    get '/categories/:id', to: 'products#by_categoryId'
-    get '/products/all', to: 'products#all'
-    get '/products/new', to: 'products#new'
-    get '/products/:id', to: 'products#view'
-    get '/category/sale', to: 'products#sale'
-    post '/customers/new', to: 'customers#create'
-    get '/provinces/view', to: 'provinces#view'
-    get '/contacts/view', to: 'contact#view'
-    get '/abouts/view', to: 'abouts#view'
+    get "/categories", to: "categories#categories"
+    get "/categories/:id", to: "products#by_categoryId"
+    get "/products/all", to: "products#all"
+    get "/products/new", to: "products#new"
+    get "/products/:id", to: "products#view"
+    get "/category/sale", to: "products#sale"
+    get "/search", to: "products#search"
+    post "/customers/new", to: "customers#create"
+    get "/provinces/view", to: "provinces#view"
+    get "/contacts/view", to: "contact#view"
+    get "/abouts/view", to: "abouts#view"
     # resources :customer, only: %i[create show index]
-    post '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
-    get '/loggedin', to: 'sessions#is_logged_in?'
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    get "/loggedin", to: "sessions#is_logged_in?"
     # resources :products
     # resources :customers
     # resources :registerations
